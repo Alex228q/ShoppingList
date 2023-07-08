b4512a0e53b7e2cb17783441c63fbb4375c8ab46
aleksei <lena45130@gmail.com>
2023-07-08 21:06:44 +0300

    init and domain layer added

---
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopListUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.shoppinglist.domain
+
+import androidx.lifecycle.LiveData
+
+class GetShopListUseCase(private val shopListRepository: ShopListRepository) {
+    fun getShopList(): List<ShopItem> {
+        return shopListRepository.getShopList()
+    }
+}
